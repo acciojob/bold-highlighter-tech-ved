172968e259f4f852fa01e0de3fa22e10a3e5e2f0
Vedant Dubey <112743896+tech-ved@users.noreply.github.com>
2026-03-03 00:12:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 function highlight() {
-    //Write your code here
-
-
+  const boldWords = document.querySelectorAll("strong");
+  boldWords.forEach(el => {
+    el.style.color = "rgb(0, 128, 0)";
+  });
 }
-
 
 function return_normal() {
-    //Write your code here
-
-    
+  const boldWords = document.querySelectorAll("strong");
+  boldWords.forEach(el => {
+    el.style.color = "rgb(0, 0, 0)";
+  });
 }
+
+
+
